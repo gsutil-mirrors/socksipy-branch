6e35c73ca6c4a69efd3673be97cb75cad84fadd7
mvilas@gmail.com <mvilas@gmail.com@9d7bb7c4-1145-faf0-e654-d8b76b9b5611>
2010-05-18 15:15:24 +0000

    improved setup script
    git-svn-id: http://socksipy-branch.googlecode.com/svn/trunk@8 9d7bb7c4-1145-faf0-e654-d8b76b9b5611

--- setup.py
+++ setup.py
@@ -7,14 +7,11 @@ setup(
 name = "SocksiPy",
 version = VERSION,
 description = "A Python SOCKS module",
-long_description = """\
-This Python module allows you to create TCP connections through a SOCKS
-proxy without any special effort.""",
+long_description = "This Python module allows you to create TCP connections through a SOCKS proxy without any special effort.",
 url = "http://socksipy.sourceforge.net/",
 author = "Dan-Haim",
 author_email="negativeiq@users.sourceforge.net",
 license = "BSD",
-packages = [""],
-platforms = ["Linux"],
+py_modules=["socks"]
 )
 
